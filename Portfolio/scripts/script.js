d80f40c0d75df32920210a062850f41801816d8a
Sabas <ssb00@iesemilidarder.com>
2023-12-11 21:00:44 +0100

    media query terminado

--- Portfolio/scripts/script.js
+++ Portfolio/scripts/script.js
@@ -1,43 +1,42 @@
-function moveDiagonal(value) {
+function moverDiagonal() {
   let diagonal = document.querySelector('.contenedorGirado');
-
-  diagonal.classList.add('move-left'); // Agrega la animación a la diagonal
+  diagonal.classList.add('move-left'); // Animacion que quita la diagonal
 }
 
-// Funciones específicas para mover cada botón a una cantidad diferente de vw
-function moveButton(buttonNumber) {
+// Funcion para mover cada botón
+function moverBtn(num) {
   for (let i = 1; i <= 5; i++) {
-    let button = document.getElementById('S' + i);
-    if (i === buttonNumber && button.classList.contains('move-left-buttons' + i + 'S')) {
-      // Si el botón ya está en el estado 'S', recargamos la página
+    let btn = document.getElementById('S' + i);
+    if (i === num && btn.classList.contains('move-left-buttons' + i + 'S')) {
+      // Si el botón ya está en el estado 'S', recargam la página
       location.reload();
       return;
-    } else if (i === buttonNumber) {
-      button.classList.add('botonS');
-      button.classList.add('move-left-buttons' + i + 'S');
-      button.classList.remove('move-left-buttons' + i);
+    } else if (i === num) {
+      btn.classList.add('botonS');
+      btn.classList.add('move-left-buttons' + i + 'S');
+      btn.classList.remove('move-left-buttons' + i);
 
-      if (buttonNumber === 1){
-        setTimeout(addNewContent1, 800);
+      if (num === 1){
+        setTimeout(añadirCont1, 800);
       }
-      if (buttonNumber === 2){
-        setTimeout(addNewContent2, 800);
+      if (num === 2){
+        setTimeout(añadirCont2, 800);
       }
-      if (buttonNumber === 3){
-        setTimeout(addNewContent3, 800);
+      if (num === 3){
+        setTimeout(añadirCont3, 800);
       }
 
     } else {
-      button.classList.add('move-left-buttons' + i);
-      button.classList.remove('move-left-buttons' + i + 'S');
-      button.classList.remove('botonS');
+      btn.classList.add('move-left-buttons' + i);
+      btn.classList.remove('move-left-buttons' + i + 'S');
+      btn.classList.remove('botonS');
     }
   }
-  moveDiagonal();
+  moverDiagonal();
 }
-function addNewContent1() {
-  const container = document.querySelector(".añadirDOM");
-  container.innerHTML = `
+function añadirCont1() {
+  const cont1 = document.querySelector(".añadirDOM");
+  cont1.innerHTML = `
       <section>
         <h1>Dice Mini Game </h1>
         <p>Minijuego que baja la vida del slime segun el número del dado</p>
@@ -51,24 +50,24 @@ function addNewContent1() {
         </form>
       </section>
   `;
-  container.classList.add('novedades');
-  container.classList.remove('proyectos');
-  container.classList.remove('contacto');
+  cont1.classList.add('novedades');
+  cont1.classList.remove('proyectos');
+  cont1.classList.remove('contacto');
 }
 
-function addNewContent2() {
+function añadirCont2() {
   const container = document.querySelector(".añadirDOM");
   container.innerHTML = `
         <section class="proyecto1">
+            <br><br>
             <a href="https://dreamy-monstera-5ac937.netlify.app"><img class="imgP1" src="img/3dJuegos.png" alt=""></a>
-            
         </section>
         <section class="proyecto2">
             <a href="https://resonant-haupia-4868f5.netlify.app/#carousel__slide1"><img class="imgP2" src="img/DescubreIbiza.png" alt=""></a>
-            
         </section>
         <section class="proyecto3">
             <a href="https://earnest-cat-cb3569.netlify.app"><img class="imgP3" src="img/DiceGameWeb.png" alt=""></a>
+            <br><br><br><br><br><br>
         </section>
   `;
   container.classList.remove('novedades');
@@ -76,7 +75,7 @@ function addNewContent2() {
   container.classList.remove('contacto');
 }
 
-function addNewContent3() {
+function añadirCont3() {
   const container = document.querySelector(".añadirDOM");
   container.innerHTML = `
     <form id="contacto" action="">
@@ -92,7 +91,7 @@ function addNewContent3() {
             <label for="texto">Mensaje</label> <br>
             <input id="texto" class="inputTexto" type="text" name="texto"> <br><br>
     
-            <input type="submit" value="Enviar">
+            <input class="button-49 btn4" type="submit" value="Enviar">
         </section>
         <section class="contactoLinks">
             <section>
